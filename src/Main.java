23142e277b3991bff78f7523b3e0d9ef9087d5f6
79273 <destal95@gmail.com>
2022-05-24 17:24:45 +0500

    Готовое д/з

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.SortedMap;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        // Задание 1
+        String firstName = "Ivan";
+        firstName = " " + firstName + " ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + firstName + middleName;
+        System.out.print("ФИО сотрудника — " + fullName);
+        System.out.println();
+
+        // Задание 2
+        System.out.print("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+        System.out.println();
+
+        // Задание 3
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.print("Данные ФИО сотрудника — " + fullName);
+
     }
 }
